74b37b04daa0b37d6ae3956f4b3eb2f50cd99bcc
BOGA\bogas <bogacnutctk@gmail.com>
2024-09-16 00:10:16 +0530

    Adding all classes to repo

--- src/main/java/DateAndTime/LocalDateExample.java
+++ src/main/java/DateAndTime/LocalDateExample.java
@@ -31,8 +31,17 @@ public class LocalDateExample {
 			System.out.println("withChronoField: "+localDate.with(ChronoField.YEAR, 2032));
 			System.out.println("withTemporal Adjusters: "+localDate.with(TemporalAdjusters.firstDayOfNextMonth()));
 			
-			//System.out.println(localDate.toEpochDay());
-			
+			System.out.println(localDate.toEpochDay());
+
+			//write new date operations
+		DateOperations dateOperations = new DateOperations();
+		dateOperations.dateOperations(localDate);
+		System.out.println(localDate);
+
+		//write to find out if its a leap year
+		
+		LeapYear leapYear = new LeapYear();
+		leapYear.leapYear(localDate);
 			
 	}
 
