43dfe3a85f38fbbc09d92f939f37987801615e40
BOGA\bogas <bogacnutctk@gmail.com>
2024-09-30 18:21:14 +0530

    Issue #16: Algo: Implement Kadane's algorithm

--- src/main/java/algo/KanadeAlgo.java
+++ src/main/java/algo/KanadeAlgo.java
@@ -1,4 +1,20 @@
 package algo;
 
 public class KanadeAlgo {
+	
+	public static void main(String[] args) {
+		int[] arr = {2,3,1,3,2,4,6,7,9,2,19};
+		
+		System.out.println(maxSubArray(arr));
+	}
+	
+	static int maxSubArray(int[] arr) {
+		int start = arr[0];
+		int end = arr[0];
+		for ( int i = 1; i < arr.length; i++) {
+			end = Math.max(arr[i], arr[i] + end);
+			start = Math.max(start, end);
+		}
+		return start;
+	}
 }
