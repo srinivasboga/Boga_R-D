64072e18e007825deafe35aa1d846089cd2984b7
BOGA\bogas <bogacnutctk@gmail.com>
2025-05-15 10:00:28 +0530

    print the non-repeated character from a string

---
+++ src/main/java/other/NonRepeatcharStr.java
@@ -0,0 +1,47 @@
+package other;
+
+import java.util.*;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class NonRepeatcharStr {
+
+	public  static void main(String[] args) {
+		
+		String str = "Non Repeating Character";
+		
+		Map<Character, Integer> map = new HashMap<>();
+		
+		Queue<Character> que = new LinkedList<>();
+		
+		/*for(char ch: str.toCharArray()) {
+			map.put(ch, map.getOrDefault(ch,0)+1);
+			que.offer(ch);
+			
+			while(!que.isEmpty() && map.get(que.peek())>1) {
+				que.poll();
+			}
+			
+			if(!que.isEmpty()) {
+				System.out.println(que.peek());
+			}
+		}*/
+		
+		
+		Optional<Character> nonRe = str.toLowerCase().chars()
+				.mapToObj(c -> (char)c)
+				.collect(Collectors.groupingBy(
+						Function.identity(), LinkedHashMap::new, Collectors.counting()
+				))
+				.entrySet()
+				.stream()
+				.filter(e -> e.getValue() == 1)
+				.map(Map.Entry::getKey)
+				.findFirst();
+		
+		nonRe.ifPresent(System.out::println);
+		
+		
+	}
+	
+}
