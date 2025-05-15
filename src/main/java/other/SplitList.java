da1f81d711af0423ce2c0421caff9e21b638429a
BOGA\bogas <bogacnutctk@gmail.com>
2025-05-15 10:54:06 +0530

    Count only alphabets, Ignore dots or special characters, Return the full list of top N frequent characters

--- src/main/java/other/SplitList.java
+++ src/main/java/other/SplitList.java
@@ -1,23 +1,44 @@
 package other;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 public class SplitList {
 	
 	public static void main(String[] args) {
-		List<Integer> List1 = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
-		int Split_Value = 3;
+		// Split a list into chunks of given size
+		List<Integer> list1 = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+		int splitValue = 3;
 		
 		List<List<Integer>> result = new ArrayList<>();
 		
-		for (int i = 0; i < List1.size(); i += Split_Value) {
-			int end = Math.min(i + Split_Value, List1.size());
-			result.add(new ArrayList<>(List1.subList(i, end)));
+		for (int i = 0; i < list1.size(); i += splitValue) {
+			int end = Math.min(i + splitValue, list1.size());
+			result.add(new ArrayList<>(list1.subList(i, end)));
 		}
 		
-		System.out.println(result);
+		System.out.println("Split List: " + result);
+		
+		// String to reversed characters (example logic correction)
+		String ss = "abjjlsdsl";
+		String reversed = new StringBuilder(ss).reverse().toString();
+		System.out.println("Reversed String: " + reversed);
+		
+		// Frequency of characters in a string (case insensitive)
+		String s = "Try programiz.pro".toLowerCase();
+		Map<Character, Integer> map = new HashMap<>();
+		
+		for (char c : s.toCharArray()) {
+			if (Character.isWhitespace(c)) continue; // skip spaces
+			map.put(c, map.getOrDefault(c, 0) + 1);
+		}
+		
+		System.out.println("Character Frequencies: " + map);
+		
+		// Find the character with maximum frequency
+		Optional<Map.Entry<Character, Integer>> maxEntry =
+				map.entrySet().stream().max(Map.Entry.comparingByValue());
+		
+		maxEntry.ifPresent(entry ->
+				System.out.println("Max Occurring Character: '" + entry.getKey() + "' with count " + entry.getValue()));
 	}
-
 }
